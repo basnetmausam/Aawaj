b42014e59d9d13c2d11935ea2948e69dae157134
Mausam Basnet <mausambasnet59@gmail.com>
2022-11-02 19:47:25 +0545

    firebase connected

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:major_try/pages/hands_on_mode/tap_page/gridview_page.dart';
 import 'package:major_try/pages/hands_on_mode/choose_option_page.dart';
@@ -6,7 +7,13 @@ import 'package:major_try/pages/hands_on_mode/typing_mode/typing_page.dart';
 import 'package:major_try/themes.dart';
 import 'package:major_try/utils/routes.dart';
 
-void main() {
+import 'firebase_options.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
